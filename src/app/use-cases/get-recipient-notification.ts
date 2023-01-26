bc72608c400229292e89060511bfe80b3677b6c7
Gustavo Henrique <gustavohsdp@gmail.com>
2023-01-26 18:14:40 -0300

    feat: :sparkles: implement GetRecipientNotifications

---
+++ src/app/use-cases/get-recipient-notification.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { Notification } from '../entities/notification';
+import { NotificationsRepository } from '../repositories/notifications.repositories';
+
+interface GetRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface GetRecipientNotificationsResponse {
+  notifications: Notification[];
+}
+
+@Injectable()
+export class GetRecipientNotifications {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(
+    request: GetRecipientNotificationsRequest,
+  ): Promise<GetRecipientNotificationsResponse> {
+    const { recipientId } = request;
+
+    const notifications =
+      await this.notificationsRepository.findManyByRecipientId(recipientId);
+
+    return { notifications };
+  }
+}
