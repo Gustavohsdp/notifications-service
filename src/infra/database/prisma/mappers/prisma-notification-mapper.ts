e2ee7cd645defa045a1f60aa087b393a59daacfe
Gustavo Henrique <gustavohsdp@gmail.com>
2023-01-26 18:42:40 -0300

    feat: :sparkles: create mapper toDomain

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,3 +1,5 @@
+import { Notification as RawNotification } from '@prisma/client';
+import { Content } from '../../../../app/entities/content';
 import { Notification } from '../../../../app/entities/notification';
 
 export class PrismaNotificationMapper {
@@ -10,5 +12,29 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    const {
+      id,
+      category,
+      canceledAt,
+      content,
+      createdAt,
+      readAt,
+      recipientId,
+    } = raw;
+
+    return new Notification(
+      {
+        category,
+        canceledAt,
+        content: new Content(content),
+        createdAt,
+        readAt,
+        recipientId,
+      },
+      id,
+    );
   }
 }
