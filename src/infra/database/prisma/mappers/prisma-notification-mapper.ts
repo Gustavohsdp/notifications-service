4d5f79a43c7d8c1e0a9f773ca7173c776a52fb3b
Gustavo Henrique <gustavohsdp@gmail.com>
2023-01-26 17:38:57 -0300

    feat: :sparkles: create mapper

---
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -0,0 +1,14 @@
+import { Notification } from '../../../../app/entities/notification';
+
+export class PrismaNotificationMapper {
+  static toPrisma(notification: Notification) {
+    return {
+      id: notification.id,
+      content: notification.content.value,
+      category: notification.category,
+      recipientId: notification.recipientId,
+      readAt: notification.readAt,
+      createdAt: notification.createdAt,
+    };
+  }
+}
