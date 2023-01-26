6606fd9b2937817eeb581ad12f9f8517acd10ada
Gustavo Henrique <gustavohsdp@gmail.com>
2023-01-26 17:40:54 -0300

    feat: :sparkles: Implement cancel notification

---
+++ src/app/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repositories';
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Cancel notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = new Notification({
+      content: new Content('Nova solicitação de amizade!'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
