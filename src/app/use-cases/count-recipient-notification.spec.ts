5dfd9d155d09058fcec8feaaee55e88033d815ba
Gustavo Henrique <gustavohsdp@gmail.com>
2023-01-26 17:56:30 -0300

    feat: :sparkles: implement CountRecipientNotifications

---
+++ src/app/use-cases/count-recipient-notification.spec.ts
@@ -0,0 +1,43 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repositories';
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+import { CountRecipientNotifications } from './count-recipient-notification';
+
+describe('Count recipients notifications', () => {
+  it('should be able to count recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotifications = new CountRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        content: new Content('Nova solicitação de amizade!'),
+        category: 'social',
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        content: new Content('Nova solicitação de amizade!'),
+        category: 'social',
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      new Notification({
+        content: new Content('Nova solicitação de amizade!'),
+        category: 'social',
+        recipientId: 'recipient-2',
+      }),
+    );
+
+    const { count } = await countRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(count).toEqual(2);
+  });
+});
